e438dfda898765f72e26a5ef8b693efb83d1bd7e
jagdeo09 <jagdeo@aclanyc.com>
2019-03-21 22:21:04 +0000

    almost done

--- js/script.js
+++ js/script.js
@@ -4,12 +4,15 @@
 /* global $ */
 
 $("#search-button").click(function(){
+    var searchInput = $("input").val();
+    console.log(searchInput);
     $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+        url: "https://api.giphy.com/v1/gifs/search?q=" + searchInput + "&rating=pg&api_key=dc6zaTOxFJmzC",
         method: "GET",
-        success: function(response) {  
-            $("body").append("<img src=" + response.images[3] + "/>");
+        success: function(response) {
+            console.log(response.data[5].images.fixed_width.url);
+            $("body").append("<img src=" + response.data[5].images.fixed_width.url + "/>");
 
-        }  
+        }
 });
 });
